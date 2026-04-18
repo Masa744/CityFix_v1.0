9561705b182b27f0724075b305e9691fcbdf229f
Masa744 <masaotovcevic03@gmail.com>
2026-04-18 15:46:06 -0400

    Create sendPush.ts

---
+++ lib/sendPush.ts
@@ -0,0 +1,15 @@
+xport async function sendPushNotification(token: string, message: string) {
+await fetch("https://exp.host/--/api/v2/push/send", {
+method: "POST",
+headers: {
+Accept: "application/json",
+"Content-Type": "application/json",
+},
+body: JSON.stringify({
+to: token,
+sound: "default",
+title: "CityFix",
+body: message,
+}),
+});
+}
